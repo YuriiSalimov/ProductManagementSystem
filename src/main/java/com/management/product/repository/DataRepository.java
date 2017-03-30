9076210fd8c9cfb4fa67f564d1663758a7687e8d
Vadym Pylypchenko <vadym_pylyp@ukr.net>
2017-03-30 21:28:42 +0300

    DataRepository refactor

--- src/main/java/com/management/product/repository/DataRepository.java
+++ src/main/java/com/management/product/repository/DataRepository.java
@@ -2,11 +2,13 @@ package com.management.product.repository;
 
 import com.management.product.entity.Model;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.NoRepositoryBean;
 
 /**
  * Interface provides a set of methods for the operation of entities with a database.
  * @param <T> Entity type, extends {@link Model}.
  * @author Вадим
  */
+@NoRepositoryBean
 public interface DataRepository<T extends Model> extends JpaRepository<T, Long> {
 }
