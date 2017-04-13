a9fd17ae557efd245046c215a95811a16e51903e
Makhinich <slav.m82@gmail.com>
2017-04-13 18:59:15 +0300

    SecurityConfig and security.properties

--- src/main/java/com/management/product/config/SecurityConfig.java
+++ src/main/java/com/management/product/config/SecurityConfig.java
@@ -1,7 +1,117 @@
 package com.management.product.config;
 
+import com.management.product.enums.UserRole;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
 /**
- * Created by Pavel on 4/11/2017.
+ * Spring configuration class.
+ *
+ * @author Slava Makhinich
+ * @version 1.0
  */
-public class SecurityConfig {
+
+@Configuration
+@EnableWebSecurity
+@ComponentScan(basePackages = "com.management.product.service")
+@PropertySource("classpath:security.properties")
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    /**
+     * Request prefix for administrators.
+     */
+    @Value("${request.admin}")
+    private String requestAdmin;
+
+    /**
+     * Request for authorization.
+     */
+    @Value("${request.login}")
+    private String requestLogin;
+
+    /**
+     * Request.access-denied-page.
+     */
+    @Value("${request.access-denied-page}")
+    private String requestAccessDeniedPage;
+
+    /**
+     * Request default success.
+     */
+    @Value("${request.default-success}")
+    String requestDefaultSuccess;
+
+    /**
+     * It is always use default success request.
+     */
+    @Value("${request.default-success.always}")
+    private boolean alwaysUseDefaultSuccess;
+
+    /**
+     * Parameter username title.
+     */
+    @Value("${parameter.username}")
+    private String parameterUsername;
+
+    /**
+     * Parameter password title.
+     */
+    @Value("${parameter.password}")
+    private String parameterPassword;
+
+    /**
+     * An instance of UserDetailsService for working with registered users.
+     */
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    /**
+     * The Method sets users access to pages of the site.
+     *
+     * @param httpSecurity An instance of {@link HttpSecurity} class
+     * @throws Exception An exception that can be thrown by HttpSecurity class methods
+     */
+    @Override
+    protected void configure(HttpSecurity httpSecurity)  throws Exception  {
+        httpSecurity
+                .logout()
+                .invalidateHttpSession(false)
+                .and()
+                .authorizeRequests()
+                .antMatchers(this.requestAdmin)
+                .hasRole(UserRole.ADMIN.name())
+                .anyRequest().permitAll()
+                .and()
+                .formLogin()
+                .loginPage(this.requestLogin)
+                .usernameParameter(this.parameterUsername)
+                .passwordParameter(this.parameterPassword)
+                .defaultSuccessUrl(this.requestDefaultSuccess, this.alwaysUseDefaultSuccess)
+                .and()
+                .exceptionHandling()
+                .accessDeniedPage(this.requestAccessDeniedPage)
+                .and()
+                .csrf().disable();
+
+    }
+
+    /**
+     * Method for setting user's roles.
+     *
+     * @param builder An instance of {@link AuthenticationManagerBuilder} class
+     * @throws Exception An exception that can be thrown by AuthenticationManagerBuilder class methods
+     */
+    @Override
+    protected void configure(final AuthenticationManagerBuilder builder) throws Exception {
+        builder.userDetailsService(this.userDetailsService);
+    }
+
 }
